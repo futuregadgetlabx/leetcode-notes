4c4ecb75fff3209c6dee90c658fc6c7b80aa9947
Cruii <crrruiii@163.com>
2019-05-20 23:15:42 +0800

    :tada: 提交第58题

---
+++ 58.最后一个单词的长度.java
@@ -0,0 +1,29 @@
+import sun.print.resources.serviceui;
+
+/*
+ * @lc app=leetcode.cn id=58 lang=java
+ *
+ * [58] 最后一个单词的长度
+ * 
+ * √ Accepted
+ * √ 59/59 cases passed (1 ms)
+ * √ Your runtime beats 98.43 % of java submissions
+ * √ Your memory usage beats 90.27 % of java submissions (34.5 MB)
+ */
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        if(s.length() == 0) {
+            return 0;
+        }
+        char[] arr = s.toCharArray();
+        int len = arr.length;
+        for (int i = len - 1; i > 0; i--) {
+            if(arr[i] == ' ' && arr[len - 1] != ' ') {
+                return len - i - 1;
+            }
+        }
+        return len;
+    }
+}
+
