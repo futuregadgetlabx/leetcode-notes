c43577647e24eb185bb90b1871c71051d695d8a8
Cruii <crrruiii@163.com>
2019-05-21 00:17:14 +0800

    :art: 优化代码

--- java/58.最后一个单词的长度.java
+++ java/58.最后一个单词的长度.java
@@ -4,20 +4,19 @@
  * [58] 最后一个单词的长度
  * 
  * √ Accepted
- * √ 59/59 cases passed (1 ms)
- * √ Your runtime beats 98.43 % of java submissions
+ * √ 59/59 cases passed (0 ms)
+ * √ Your runtime beats 100 % of java submissions
  * √ Your memory usage beats 90.27 % of java submissions (34.5 MB)
  */
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();
-        if(s.length() == 0) {
+        if(s.length() == 0 || "".equals(s)) {
             return 0;
         }
-        char[] arr = s.toCharArray();
-        int len = arr.length;
-        for (int i = len - 1; i > 0; i--) {
-            if(arr[i] == ' ' && arr[len - 1] != ' ') {
+        s = s.trim();
+        int len = s.length();
+        for (int i = len - 1; i >= 0; i--) {
+            if(s.charAt(i) == ' ') {
                 return len - i - 1;
             }
         }
