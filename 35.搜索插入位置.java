0f6e41a5b5d1b230bd6aa61147483f0b2a56ef39
Cruii <crrruiii@163.com>
2019-05-20 11:27:12 +0800

    :tada: 提交第35题

---
+++ 35.搜索插入位置.java
@@ -0,0 +1,28 @@
+import com.sun.org.apache.xml.internal.serializer.ElemDesc;
+
+/*
+ * @lc app=leetcode.cn id=35 lang=java
+ *
+ * [35] 搜索插入位置
+ */
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(target == 0 || nums.length == 0 || target <= nums[0]) {
+            return 0;
+        }
+
+        int start = 0;
+        int end = nums.length;
+        int middle;
+        while(start < end) {
+            middle = ((end - start) >> 1) + start;
+            if (target > nums[middle]) {
+                start = middle + 1;
+            } else {
+                end = middle;
+            }
+        }
+        return end;
+    }
+}
+
