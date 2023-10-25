63f6f4e3e59d7cc834faf805e103c4780fc5c605
cruii <cruii811@gmail.com>
2023-10-26 01:28:54 +0800

    submit cases

--- cruii/20.有效的括号.go
+++ cruii/20.有效的括号.go
@@ -6,7 +6,36 @@
 
 // @lc code=start
 func isValid(s string) bool {
+	n := len(s)
+	if n%2 == 1 {
+		return false
+	}
 
+	tab := map[byte]byte{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+
+	stack := make([]byte, 0)
+	for i := 0; i < n; i++ {
+		// 是否右括号
+		if tab[s[i]] > 0 {
+			// 1. 是栈底则无法正确闭合
+			// 2. 括号是否配对
+			if len(stack) == 0 || stack[len(stack)-1] != tab[s[i]] {
+				return false
+			}
+			// 配对则弹出左括号
+			stack = stack[:len(stack)-1]
+		} else {
+			// 压入左括号
+			stack = append(stack, s[i])
+		}
+	}
+
+	return len(stack) == 0
 }
+
 // @lc code=end
 
